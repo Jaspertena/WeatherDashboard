3226fc9e2e54e9eb9a38dc0069a8d7184e99089f
Maddox Twin1 <jaspertena@aol.com>
2022-04-05 00:11:11 -0400

    HTML setup

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-var APIKey = cf03651acb889096c89d126b31fba9f0
+/* =========================================================================
+ * Identifying different DOM elements
+ * ========================================================================= */
+console.log("is this connected")
+
+// var APIKey = cf03651acb889096c89d126b31fba9f0;
+// var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=";
+
+// console.log(api.openweathermap.org/data/2.5/forecast?q=Atlanta&appid=APIKey
+/* =========================================================================
+ * Identifying different DOM elements
+ * ========================================================================= */
+function GetWeather () {
+    const newName= document.getElementById("citySearch");
+    const cityName= document.getElementById("cityName");
+    
+}
+
+/* =========================================================================
+ * Library of questions and answers as objects
+ * ========================================================================= */
