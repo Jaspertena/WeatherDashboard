b7f498177e36445a5539f8095033664fd3f7e1f2
Maddox Twin1 <jaspertena@aol.com>
2022-04-12 19:10:34 -0400

    add local storage

--- Assets/script.js
+++ Assets/script.js
@@ -5,16 +5,57 @@ console.log("is this connected")
 
 var APIKey = "5552027f7503c27427f8a590ee79ad38";
 
+var current = moment().format('dddd MMMM DD, YYYY');
+
+function setDate () {
+    var now = document.querySelector("#currentDay");
+    now.textContent=current;
+}
+setDate();
+
 
 var searchButton = document.getElementById("search");
 var inputSearch = document.getElementById("inputSearch");
+var cityList = []
 
+var buttonContainer = document.querySelector(".buttonContainer")
 searchButton.addEventListener("click", function(event){
     event.preventDefault();
     
-    getCoordinate(inputSearch.value)
+    getCoordinate(inputSearch.value);
+    cityList.push(inputSearch.value);
+    console.log(cityList);
+    var cityBtn = document.createElement("button");
+    cityBtn.textContent = inputSearch.value;
+    cityBtn.classList.add("btn","btn-primary","mb-2", "col-8");
+    buttonContainer.appendChild(cityBtn);
+    cityBtn.addEventListener("click", function(event){
+        event.preventDefault();
+        getCoordinate(cityBtn.textContent)
+    })
+    saveCity()
 })
 
+    function saveCity() {
+    for (var i = 0; i <cityList.length; i++) {
+        localStorage.setItem(i,cityList[i])
+    }
+}
+function getCity (){
+    for (var i = 0; i <localStorage.length; i++) {
+        var keyValue=localStorage.key(i)
+        var city=localStorage.getItem(keyValue)
+        var cityBtn = document.createElement("button");
+        cityBtn.textContent = city;
+        cityBtn.classList.add("btn","btn-primary","mb-2", "col-8");
+        buttonContainer.appendChild(cityBtn);
+        cityBtn.addEventListener("click", function(event){
+            event.preventDefault();
+            getCoordinate(city)
+        })
+    }
+}
+getCity()
 const newName= document.getElementById("citySearch");
 const cityName= document.getElementById("cardTitle");
 
@@ -25,21 +66,25 @@ function getCoordinate(city){
         .then(function(response){
             response.json()
             .then(function(data){
-                getCurrentWeather(data[0].lat, data[0].lon);
+                getCurrentWeather(data[0].lat, data[0].lon, data[0].name);
                 getForecastWeather(data[0].lat, data[0].lon)
+                console.log(data)
+
             })
         })
 }
 
 
-function getCurrentWeather (lat, lon) {
+function getCurrentWeather (lat, lon, city) {
     var baseURL = "https://api.openweathermap.org/data/2.5/onecall?lat=";
     var rest = "&lon=";
     var rest2 = "&units=imperial&exclude=minutely,hourly,daily,alerts&appid=";
+    var searchCity = city
     fetch (baseURL + lat + rest + lon + rest2 + APIKey)
         .then(function(response){
             response.json()
-            .then(function(data){displayCurrentData(data)})
+            .then(function(data){displayCurrentData(data,searchCity)})
+
         })
 }
 
@@ -58,11 +103,12 @@ function getForecastWeather (lat, lon) {
 var getCurrentWeatherContainer = document.querySelector(".currentContainer")
 
 
-function displayCurrentData(data) {
+function displayCurrentData(data, city) {
+    getCurrentWeatherContainer.innerHTML=""
     var weatherCard = document.createElement("div")
     weatherCard.setAttribute("class", "card")
     var cityName = document.createElement("h1")
-    cityName.textContent=inputSearch.value
+    cityName.textContent=city
     cityName.setAttribute("class", "text-center")
     weatherCard.appendChild(cityName)
     var currentTemp = document.createElement("h4")
@@ -90,6 +136,7 @@ function displayCurrentData(data) {
 var forecastContainer = document.querySelector(".forecastContainer")
 
 function displayForecastData(data) {
+    forecastContainer.innerHTML = ""
     for(i=1; i<6; i++){
         var weatherCard = document.createElement("div")
     weatherCard.setAttribute("class", "card")
